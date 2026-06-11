e6fcadc9d0d6e429965aa9fbc219be40b2e93f61
maciek22002-90@o2.pl <macrze001@pbs.edu.pl>
2026-06-11 20:20:25 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite-plus";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [react(), tailwindcss()],
+});
